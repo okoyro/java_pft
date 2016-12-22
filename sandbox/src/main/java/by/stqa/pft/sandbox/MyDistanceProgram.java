1ba8606f47e7c1149dc0e148aeaaccdf47ed6782
okoyro <oksana.koyro@gmail.com>
2016-12-22 09:59:49 +0300

    Добавлен метод calcDistance

--- sandbox/src/main/java/by/stqa/pft/sandbox/MyDistanceProgram.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/MyDistanceProgram.java
@@ -2,18 +2,18 @@ package by.stqa.pft.sandbox;
 
 public class MyDistanceProgram {
   public static void main(String[] args) {
-    Point p1 = new Point(5, 4);
+    Point p1 = new Point(1, 2);
     //        p1.x = 2;  all attributes are filled out in constructor
     //        p1.y = 3;
 
-    Point p2 = new Point(7, 8);
+    Point p2 = new Point(3, 4);
     //        p2.x = 4;  all attributes are filled out in constructor
     //        p2.y = 5;
 
-    System.out.println("Расстояние между точками = " + distance(p1, p2));
+    System.out.println("Расстояние между точками = " + p1.calcDistance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.y - p1.y), 2) + Math.pow((p2.x - p1.x), 2));
-  }
+  //  public static double distance(Point p1, Point p2) {
+  //    return Math.sqrt(Math.pow((p2.y - p1.y), 2) + Math.pow((p2.x - p1.x), 2));
+  //  } there is a static function which can be used to calculate the distance (need to add params: distance(p1,p2))
 }
--- sandbox/src/main/java/by/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/Point.java
@@ -9,4 +9,10 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double calcDistance(Point p2) {
+    return Math.sqrt(Math.pow((this.y - p2.y), 2) + Math.pow((this.x - p2.x), 2));
+  }
+  //  1) один объект -- тот, в котором вызывается метод, он внутри метода доступен под специальным именем this
+  //  2) второй объект -- передаётся в метод как значение параметра
 }
