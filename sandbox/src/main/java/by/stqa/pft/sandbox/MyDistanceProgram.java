9f6f8c0a92fbfc758ed4dedadb0f424f8774cced
okoyro <oksana.koyro@gmail.com>
2016-12-22 09:17:16 +0300

    Code refactoring (Math.pow function)

--- sandbox/src/main/java/by/stqa/pft/sandbox/MyDistanceProgram.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/MyDistanceProgram.java
@@ -14,7 +14,6 @@ public class MyDistanceProgram {
   }
 
   public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p2.y - p1.y) * (p2.y - p1.y)) + ((p2.x - p1.x) * (p2.x - p1.x)));
-
+    return Math.sqrt(Math.pow((p2.y - p1.y), 2) + Math.pow((p2.x - p1.x), 2));
   }
 }
