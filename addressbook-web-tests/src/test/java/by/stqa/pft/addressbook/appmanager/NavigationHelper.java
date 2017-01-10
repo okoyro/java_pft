0a31fd90208ade27b8d112bfca346aab0bc9e3ee
okoyro <oksana.koyro@gmail.com>
2017-01-10 15:56:14 +0300

    Оптимизирован метод gotoNewContactPage

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -19,6 +19,10 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoNewContactPage() {
+    if (isElementPresent(By.tagName("h1"))
+        && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    }
     clickLink("http://localhost/addressbook/edit.php");
   }
 
