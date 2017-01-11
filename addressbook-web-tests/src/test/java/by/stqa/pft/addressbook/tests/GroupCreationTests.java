28f0b05eef76e118e6a2252d70c9ac06ec8b0bc2
okoyro <oksana.koyro@gmail.com>
2017-01-11 11:44:34 +0300

    Оптимизирован testGroupCreation

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,11 +8,12 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-//    app.getGroupHelper().createGroup(new GroupData("test1", null, null));   ability to use this method to create group
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+//    there is an ability to use one method or a few methods to create group
+//    app.getGroupHelper().initGroupCreation();
+//    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
+//    app.getGroupHelper().submitGroupCreation();
+//    app.getGroupHelper().returnToGroupPage();
   }
 
 }
