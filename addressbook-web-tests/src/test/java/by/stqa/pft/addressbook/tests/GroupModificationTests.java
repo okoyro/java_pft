62f47188aa4730ebbbf0261ea6d7bf7c63f873cd
okoyro <oksana.koyro@gmail.com>
2017-01-21 11:35:51 +0300

    Задан параметр выбора последней группы для теста

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,9 +13,9 @@ public class GroupModificationTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", null));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1(ed)", "test2(ed)", null));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
