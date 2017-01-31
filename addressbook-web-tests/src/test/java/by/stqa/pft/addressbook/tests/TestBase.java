16f1599cd7f2dd6440589469f85188852f1db348
okoyro <oksana.koyro@gmail.com>
2017-01-31 11:15:18 +0300

    Реализован запуск всех тестов для групп с запуском браузера один раз
    Используется аннотация @BeforeSuite.

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
@@ -2,19 +2,20 @@ package by.stqa.pft.addressbook.tests;
 
 import by.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX); //этот метод должен быть static для
+  //доступа к переменной "app" из всех тестовых классов
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
