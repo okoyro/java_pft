e6b58418d7175b49f9b16f3e02aea97e831a0315
okoyro <oksana.koyro@gmail.com>
2017-01-31 10:05:46 +0300

    Реализован поиск максимального id, т.к. у вновь созданной группы id имеет наибольшее числовое значение. Сравнены множества с учетом id, firstname & lastname, но без учета порядка

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import by.stqa.pft.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -11,15 +12,24 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.getNavigationHelper().gotoHomePage();
-    List<ContactData>  before = app.getContactHelper().getContactList();
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoNewContactPage();
-    app.getContactHelper().fillNewContactForm
-            (new ContactData("Jane", "Q", "Poi", "title", "company",
-                             "987-654-32", "test@test.test", null), true);
+    ContactData contact = new ContactData("Jane", "Q", "Poi", "title", "company",
+                                          "987-654-32", "test@test.test", null);
+    app.getContactHelper().fillNewContactForm(contact, true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().gotoHomePage();
-    List<ContactData>  after = app.getContactHelper().getContactList();
+    List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    app.getNavigationHelper().gotoHomePage();
+
+    int max = 0;
+    for (ContactData c : after) {
+      if (c.getId() > max) {
+        max = c.getId();
+      }
+    }
+    contact.setId(max);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
