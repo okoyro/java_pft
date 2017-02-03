d9e958e4958a06aa1bbe444a54a125a5b30d70f6
okoyro <oksana.koyro@gmail.com>
2017-02-03 16:27:40 +0300

    Реализовано сравнение множеств в testGroupCreation

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -90,4 +92,15 @@ public class GroupHelper extends HelperBase {
     }
     return groups;
   }
+
+  public Set<GroupData> all() {
+      Set<GroupData> groups = new HashSet<GroupData>();
+      List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+      for (WebElement element : elements) {
+        String name = element.getText();
+        int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+        groups.add(new GroupData().withId(id).withName(name));
+      }
+      return groups;
+    }
 }
--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,47 +4,21 @@ import by.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().gotoGroupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("test_X");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    // для проверки прогоняем все элементы списка по циклу
-    //находим группу с максимальным числом в параметре id, т.к. справедливо предположить, что у только что созданной
-    //группы будет именно такой id
-
-    //    int max = 0;
-    //    for (GroupData g : after) {
-    //      if (g.getId() > max) {
-    //        max = g.getId();
-    //      }
-    //    }
-
-    //    список превращен в поток, по нему пробегает функция compare и сравниваются объекты типа GroupData (по id)
-    //    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-
-    //добавляем ту же группу, которую добавили в тесте
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
-
-
-    //    there is an ability to use one method or a few methods to create group
-    //    app.group().initGroupCreation();
-    //    app.group().fillGroupForm(new GroupData("test1", null, null));
-    //    app.group().submitGroupCreation();
-    //    app.group().returnToGroupPage();
   }
-
 }
