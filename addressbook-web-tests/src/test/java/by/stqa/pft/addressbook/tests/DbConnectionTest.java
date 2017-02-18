2c8d7f2fc5d5cd5bc191768fdc9fdce8f771c9ed
okoyro <oksana.koyro@gmail.com>
2017-02-18 15:44:20 +0300

    Реализовано подключение к БД и чтение данных из таблицы групп

---
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,40 @@
+package by.stqa.pft.addressbook.tests;
+
+import by.stqa.pft.addressbook.model.GroupData;
+import by.stqa.pft.addressbook.model.Groups;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnectionTest {
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?serverTimezone=UTC&"
+                                         + "user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      Groups groups = new Groups();
+      while (rs.next()) {
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                           .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+
+      System.out.println(groups);
+    }
+    catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
