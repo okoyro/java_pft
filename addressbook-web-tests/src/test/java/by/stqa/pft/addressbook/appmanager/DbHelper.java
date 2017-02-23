02bfa4c889d00a2a44988d5f7f07c9979930e9dd
okoyro <oksana.koyro@gmail.com>
2017-02-23 09:39:52 +0300

    Создан метод для полусения списков контактов из БД

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/DbHelper.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/appmanager/DbHelper.java
@@ -1,6 +1,7 @@
 package by.stqa.pft.addressbook.appmanager;
 
 import by.stqa.pft.addressbook.model.ContactData;
+import by.stqa.pft.addressbook.model.Contacts;
 import by.stqa.pft.addressbook.model.GroupData;
 import by.stqa.pft.addressbook.model.Groups;
 import org.hibernate.Session;
@@ -29,5 +30,14 @@ public class DbHelper {
     session.getTransaction().commit();
     session.close();
     return new Groups(result);
+  }
+
+  public Contacts contacts() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<ContactData> result = session.createQuery("from ContactData where deprecated = '0000-00-00'").list();
+    session.getTransaction().commit();
+    session.close();
+    return new Contacts(result);
   }
 }
