94c5237f2a4b2409ecc8d89619ae4d2dc8d31a96
okoyro <oksana.koyro@gmail.com>
2017-02-26 17:58:31 +0300

    Тест регистрации закончен. Реализована отправка письма на SMTP server.

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -9,3 +9,6 @@ $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = '65O1VeTsdBfI46ECqfBpYmQkdfJjV999I60VVx3ZaZg=';
 $g_signup_use_captcha=OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+$g_smtp_port = '2222';
